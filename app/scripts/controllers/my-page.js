062b5947b85972b2ab308894839e4f5ffc597289
Jaehoon Lee <alphahoon@gmail.com>
2017-06-04 18:29:38 +0900

    user deletion implemented

--- app/scripts/controllers/my-page.js
+++ app/scripts/controllers/my-page.js
@@ -8,12 +8,43 @@
  * Controller of the SportsGeeksApp
  */
 angular.module('SportsGeeksApp')
-    .controller('MyPageCtrl', ['States', function (States) {
+    .controller('MyPageCtrl', ['$http', '$location', 'Config', 'States', function ($http, $location, Config, States) {
         States.setCurrentPage(8);
+        var store = this;
         this.isLoggedIn = function () {
             return States.isLoggedIn();
         };
         this.username = function () {
             return States.username();
         };
+        this.deleteAccount = function () {
+            var tmpUsername = States.username();
+            var tmpPassword = States.password();
+            var tmpToken = States.token();
+            States.logout();
+
+            $http({
+                    url: Config.url + 'delete-account',
+                    method: 'POST',
+                    params: {
+                        apiKey: Config.apiKey,
+                        token: tmpToken
+                    },
+                    data: {
+                        username: tmpUsername,
+                        password: tmpPassword
+                    }
+                })
+                .then(function (res) {
+                    console.log('Successfully deleted account!');
+                    store.status = res.data;
+                    console.log(store.status);
+                    $location.path('/');
+                }, function (res) {
+                    console.log('Error while deleting account!');
+                    store.status = res.data;
+                    console.log(store.status);
+                    $location.path('/');
+                });
+        }
     }]);
